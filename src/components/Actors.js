a45d4cf06c28777da90a44fdc2df166fcdf60760
tchartchke <chartai.kl@gmail.com>
2021-06-14 15:44:02 -0500

    commit tree 134be0ddddecbb5c8cea0c5d79afd844d28c1329

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { actors } from '../data';
 
+const actorsCard = () => {
+  return (
+    actors.map(actors => 
+    <div>
+      <h2>Name: {actors.name}</h2>
+      <p>Movies:</p>
+      <ul>
+        {actors.movies.map(movie => <li>{movie}</li>)}
+      </ul>
+    </div>)
+  )
+}
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Actors Page</h1>
+      {actorsCard()}
     </div>
   );
 };
