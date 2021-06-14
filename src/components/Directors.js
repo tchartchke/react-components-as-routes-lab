e92cfd9450bab86014d5fd57f0b8eddd6065f96b
tchartchke <chartai.kl@gmail.com>
2021-06-14 15:44:04 -0500

    done

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { directors } from '../data';
 
+const directorCard = () => {
+  return (
+    directors.map(director => 
+    <div>
+      <h2>Name: {director.name}</h2>
+      <p>Movies:</p>
+      <ul>
+        {director.movies.map(movie => <li>{movie}</li>)}
+      </ul>
+    </div>)
+  )
+}
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorCard()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { movies } from '../data';
 
+const movieCards = () => {
+  return (
+    movies.map(movie => 
+    <div>
+      <h2>Name: {movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <p>Genres: 
+        <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+        </ul>
+      </p>
+    </div>)
+  )
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movieCards()}
     </div>
   );
 };
